bf6abf2aa8d810d66a088af88e9ef435849d4f3c
karim wydadi <don.karimmansour@gmail.com>
2022-01-13 14:24:48 +0000

    make it better

--- versions/v1.js
+++ versions/v1.js
@@ -19,6 +19,8 @@ app.use((req, res, next) => {
     res.status(404).json("Api not found")
 })
 
+require("dotenv").config()
+
 app.listen(process.env.PORT || 8080, () => {
     console.log("server start")
 })
