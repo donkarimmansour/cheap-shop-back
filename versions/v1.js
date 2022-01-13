30464aae37a6d07cd8e57a681887565919e20061
karim wydadi <don.karimmansour@gmail.com>
2022-01-13 15:11:20 +0000

    make it better

--- versions/v1.js
+++ versions/v1.js
@@ -19,6 +19,6 @@ app.use((req, res, next) => {
     res.status(404).json("Api not found")
 })
 
-app.listen(process.env.PORT || 8080, () => {
+app.listen(process.env.PORT || 3001 , () => {
     console.log("server start")
 })
