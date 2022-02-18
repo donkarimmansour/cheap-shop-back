7ddb8a952923f551c6c3731a58d3d41d13c84a30
karim wydadi <don.karimmansour@gmail.com>
2022-02-18 17:29:35 +0000

    make it better

--- common/messages.js
+++ common/messages.js
@@ -20,7 +20,7 @@ const confimEmailMsg = (id) => {
     <h3>Please Verify Your Email by clicking the following link</h3>
     <br><br>
     <a style='text-decoration:none;' 
-     href="https://ectestone.herokuapp.com/login/${id}">
+     href="http://dreams-shop.herokuapp.com/login/${id}">
     Click Here to confirm your email</a>` ;
     return html ;
 }
